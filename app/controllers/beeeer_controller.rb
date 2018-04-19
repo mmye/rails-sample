47f4ce58a2c1b5ce9cd8f19abe102ae31b3fd352
masato maeyama <>
2018-04-19 15:00:15 +0900

    add search function

--- app/controllers/beeeer_controller.rb
+++ app/controllers/beeeer_controller.rb
@@ -3,4 +3,8 @@ class BeeeerController < ApplicationController
   def index
 		@beers = Beeeer.order('item_name ASC').page(params[:page]).per(20)
   end
+
+	def search
+		@beers = Beeeer.where('item_name LIKE(?)', "%#{params[:keyword]}%").order('item_name ASC').page(params[:page]).per(20)
+	end
 end
